28af494fefe817171c44eaa974323ba2a8310bff
touhid1 <touhidulislam066.com>
2024-01-25 23:25:43 +0600

    DropdownTest

--- src/test/java/com/touhidul/TestWebElement.java
+++ src/test/java/com/touhidul/TestWebElement.java
@@ -1,11 +1,6 @@
 package com.touhidul;
-
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class TestWebElement extends DriverSetup{
